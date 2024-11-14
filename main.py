950458dcedfd4b59172ac323506e774417b62a01
Pkrishon <Kpinkins@loyola.edu>
2024-11-14 12:42:39 -0500

    Updated code

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# Import os package into the code
+import os
+
+# Name: read_file_name
+# Parameters: None
+# Return: f_name
+# Processes user's input and output's if user's input is invalid
+def read_file_name():
+    f_name = input("Enter file name: ")
+    while not os.path.isfile(f_name):
+        f_name = input("File not exist. Enter file name: ")
+    return f_name
+
+
+
