e5112cd3ec41f8cbd67ce0dd306031034f3e1334
Pkrishon <Kpinkins@loyola.edu>
2024-11-14 13:09:08 -0500

    Updated code

--- main.py
+++ main.py
@@ -12,5 +12,27 @@ def read_file_name():
         f_name = input("File not exist. Enter file name: ")
     return f_name
 
+def read_file(file_name):
+    movie_table = []
+    try:
+        file = open(file_name, "r")
+        for line in file:
+            row = line.split()
+            row[3] = int(row[3])
+            row[4] = int(row[4])
+            row[5] = int(row[5])
+            for i in range(len(line)):
+                if row[i].isdigit():
+                    movie_table.append(row)
+                return movie_table
+    except FileNotFoundError:
+        print("File not found.")
+        return
 
+def main():
+    file_name = read_file_name()
+    read_file(file_name)
 
+    return
+
+main()
